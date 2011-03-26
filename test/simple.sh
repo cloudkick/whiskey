4ceb3cd85c9205f9b44f3d12e3f8597df52b125a
Tomaz Muraus <tomaz@tomaz.me>
2011-03-26 03:15:09 +0100

    Add test for timeout.

--- test/simple.sh
+++ test/simple.sh
@@ -14,3 +14,12 @@ if [ ! $? -gt 0 ]; then
     echo "Test should fail but it passed"
     exit 1
 fi
+
+${CWD}/bin/whiskey --timeout 1000 --tests "${CWD}/example/test-timeout.js"
+
+if [ ! $? -eq 1 ]; then
+    echo "Test should time out but it didn't."
+    exit 1
+fi
+
+exit 0
