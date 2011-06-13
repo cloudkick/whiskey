294f04e14f5f7a818a8907380b302a5177a3edda
Tomaz Muraus <tomaz@tomaz.me>
2011-06-13 10:40:23 +0200

    Add basic test for assert.response.

--- test/run.sh
+++ test/run.sh
@@ -235,6 +235,14 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+"${CWD}/bin/whiskey" --timeout 1000 \
+ --tests "${CWD}/example/test-custom-assert-methods.js"
+
+if [ $? -ne 0 ]; then
+    echo "test didn't exit with zero exit code"
+    exit 1
+fi
+
 echo ""
 echo "* * * Whiskey test suite PASSED. * * *"
 exit 0
